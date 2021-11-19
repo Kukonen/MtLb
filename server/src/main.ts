b8ad1c33de32962d2ebd6754eda13b65aeec384b
Evgeniy <ekukonen@gmail.com>
2021-11-19 23:23:15 +0300

    adding dependencies

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  await mongoose.connect('mongodb://localhost:27017/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'test',
+  } as mongoose.ConnectOptions);
   await app.listen(3000);
 }
 bootstrap();
